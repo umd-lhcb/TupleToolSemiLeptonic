2c98d285671ff238f214e73654c0016d0c1db104
Yipeng Sun <syp@umd.edu>
2019-01-04 01:57:46 -0500

    Add clang-format reformatted files.

--- TupleToolApplyIsolation.h
+++ TupleToolApplyIsolation.h
@@ -4,14 +4,13 @@
 // Include files
 // from Gaudi
 #include "DecayTreeTupleBase/TupleToolBase.h"
-#include "Kernel/IParticleTupleTool.h"            // Interface
-#include "TrackInterfaces/ITrackVertexer.h"
-#include "Kernel/Particle2MCLinker.h"
 #include "Kernel/IParticle2MCAssociator.h"
+#include "Kernel/IParticleTupleTool.h" // Interface
+#include "Kernel/Particle2MCLinker.h"
+#include "TrackInterfaces/ITrackVertexer.h"
 //#include "LoKi/DistanceCalculator.h"
 //#include "LoKi/DistanceCalculatorBase.h"
 //#include "ChargedParticleMakerBase.h"
-
 
 #include "TMVA/Reader.h"
 #include "TString.h"
@@ -22,10 +21,9 @@ class IVertexFit;
 class IPVReFitter;
 
 namespace LHCb {
-  class Particle;
-  class Vertex;
+class Particle;
+class Vertex;
 };
-
 
 /** @class TupleToolVtxIsoln TupleToolVtxIsoln.h 
  *
@@ -45,46 +43,38 @@ namespace LHCb {
  */
 class TupleToolApplyIsolation : public TupleToolBase, virtual public IParticleTupleTool {
 public:
-  /// Standard constructor
-  TupleToolApplyIsolation( const std::string& type,
-		     const std::string& name,
-		     const IInterface* parent);
-  
-  virtual ~TupleToolApplyIsolation( ){}; ///< Destructor
-  
-  virtual StatusCode initialize();
-  
-  StatusCode fill( const LHCb::Particle*
-		   , const LHCb::Particle*
-		   , const std::string&
-		   , Tuples::Tuple& );
+    /// Standard constructor
+    TupleToolApplyIsolation(const std::string& type,
+        const std::string& name,
+        const IInterface* parent);
+
+    virtual ~TupleToolApplyIsolation() {}; ///< Destructor
+
+    virtual StatusCode initialize();
+
+    StatusCode fill(const LHCb::Particle*, const LHCb::Particle*, const std::string&, Tuples::Tuple&);
 
 protected:
-
 private:
+    Float_t opening, minipchi2, newfdchi2, oldfdchi2, ghostprob, trackchi2, deltafd, pt, ip, chi2, type, vertexchi2, Dst_PT, dummy;
 
+    bool isTrackInDecay(const LHCb::Track*, std::vector<const LHCb::Track*>);
+    double getminipchi(const LHCb::Particle*);
+    double getfdchi2(const LHCb::Track*, LHCb::Vertex);
+    double getopening(const LHCb::Track*, const LHCb::Particle*);
+    const LHCb::Vertex* originVertex(const LHCb::Particle*, const LHCb::Particle*) const;
+    IDVAlgorithm* m_dva;
 
-    
-  Float_t opening,minipchi2,newfdchi2,oldfdchi2,ghostprob,trackchi2,deltafd,pt,ip,chi2,type,vertexchi2,Dst_PT,dummy;
-
-  bool isTrackInDecay(const LHCb::Track*, std::vector<const LHCb::Track*>);
-  double getminipchi(const LHCb::Particle*);
-  double getfdchi2(const LHCb::Track*, LHCb::Vertex);
-  double getopening(const LHCb::Track*,const  LHCb::Particle*);  
-  const LHCb::Vertex* originVertex( const  LHCb::Particle*
-				    , const LHCb::Particle* ) const;
-  IDVAlgorithm* m_dva;
-
-  IDistanceCalculator *m_dist;
-  const IVertexFit* m_pVertexFit;
-  IParticle2MCAssociator *m_p2mcAssoc;
-  IPVReFitter* m_pvReFitter;
-  TMVA::Reader *m_Reader;
-  double m_deltaChi2; 
-  double m_Chi2; 
-  std::string m_typeVertexFit; 
-  std::string m_outputSuffix; 
-  std::string m_weightsName; 
-  std::vector<std::string> m_inputParticles;
+    IDistanceCalculator* m_dist;
+    const IVertexFit* m_pVertexFit;
+    IParticle2MCAssociator* m_p2mcAssoc;
+    IPVReFitter* m_pvReFitter;
+    TMVA::Reader* m_Reader;
+    double m_deltaChi2;
+    double m_Chi2;
+    std::string m_typeVertexFit;
+    std::string m_outputSuffix;
+    std::string m_weightsName;
+    std::vector<std::string> m_inputParticles;
 };
 #endif // TupleToolApplyIsolation_H
--- TupleToolTagDiscardDstMu.h
+++ TupleToolTagDiscardDstMu.h
@@ -4,7 +4,7 @@
 // Include files
 // from Gaudi
 #include "DecayTreeTupleBase/TupleToolBase.h"
-#include "Kernel/IParticleTupleTool.h"            // Interface
+#include "Kernel/IParticleTupleTool.h" // Interface
 
 class IDVAlgorithm;
 class IDistanceCalculator;
@@ -12,8 +12,8 @@ class IVertexFit;
 class ILifetimeFitter;
 
 namespace LHCb {
-  class Particle;
-  class Vertex;
+class Particle;
+class Vertex;
 }
 
 /** @class TupleToolTagDiscardDstMu TupleToolTagDiscardDstMu.h
@@ -33,61 +33,52 @@ namespace LHCb {
  *  @date   2008-04-15
  */
 class TupleToolTagDiscardDstMu : public TupleToolBase,
-                          virtual public IParticleTupleTool
-{
+                                 virtual public IParticleTupleTool {
 
 public:
+    /// Standard constructor
+    TupleToolTagDiscardDstMu(const std::string& type,
+        const std::string& name,
+        const IInterface* parent);
 
-  /// Standard constructor
-  TupleToolTagDiscardDstMu( const std::string& type,
-                     const std::string& name,
-                     const IInterface* parent);
+    virtual ~TupleToolTagDiscardDstMu() {}; ///< Destructor
 
-  virtual ~TupleToolTagDiscardDstMu( ){}; ///< Destructor
+    virtual StatusCode initialize();
 
-  virtual StatusCode initialize();
-
-  StatusCode fill( const LHCb::Particle*
-                   , const LHCb::Particle*
-                   , const std::string&
-                   , Tuples::Tuple& );
+    StatusCode fill(const LHCb::Particle*, const LHCb::Particle*, const std::string&, Tuples::Tuple&);
 
 private:
-
-  const LHCb::Vertex* originVertex( const  LHCb::Particle*
-                                    , const LHCb::Particle* ) const;
+    const LHCb::Vertex* originVertex(const LHCb::Particle*, const LHCb::Particle*) const;
 
 private:
-
-  IDVAlgorithm* m_dva;
-  const IDistanceCalculator* m_dist;
-  const IVertexFit* m_pVertexFit;
-  ILifetimeFitter* m_ltfit; 
-  std::string m_outputSuffix;
-  int m_pri1;
-  int m_pri2;
-  int m_pri3;
-  int m_sec1;
-  int m_sec2;
-  int m_sec3;
-  int m_ter1;
-  int m_ter2;
-  int m_ter3;
-  int m_priDOCA;
-  int m_secDOCA;
-  int m_terDOCA;
-  int m_priexDOCA1;
-  int m_secexDOCA1;
-  int m_terexDOCA1;
-  int m_priexDOCA2;
-  int m_secexDOCA2;
-  int m_terexDOCA2;
-  int m_TimePID;
-  bool m_Dstar;
-  bool m_discSlow;
-  std::string m_typeVertexFit;
-  std::vector<std::string> m_inputParticles;
-
+    IDVAlgorithm* m_dva;
+    const IDistanceCalculator* m_dist;
+    const IVertexFit* m_pVertexFit;
+    ILifetimeFitter* m_ltfit;
+    std::string m_outputSuffix;
+    int m_pri1;
+    int m_pri2;
+    int m_pri3;
+    int m_sec1;
+    int m_sec2;
+    int m_sec3;
+    int m_ter1;
+    int m_ter2;
+    int m_ter3;
+    int m_priDOCA;
+    int m_secDOCA;
+    int m_terDOCA;
+    int m_priexDOCA1;
+    int m_secexDOCA1;
+    int m_terexDOCA1;
+    int m_priexDOCA2;
+    int m_secexDOCA2;
+    int m_terexDOCA2;
+    int m_TimePID;
+    bool m_Dstar;
+    bool m_discSlow;
+    std::string m_typeVertexFit;
+    std::vector<std::string> m_inputParticles;
 };
 
 #endif // MPATEL_TUPLETOOLGEOMETRY_H
