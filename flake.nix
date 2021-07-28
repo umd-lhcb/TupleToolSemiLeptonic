1658ca47a6d61c987ed72627480eb1b30c78ac1e
Yipeng Sun <syp@umd.edu>
2021-07-28 02:09:02 +0200

    Add a wrapper script for executing a DV docker with CLion inside.

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
           ];
 
           shellHook = ''
-            export PATH=$(pwd)/test:$(pwd)/tools:$(pwd)/ganga:$(pwd)/scripts:$PATH
+            export PATH=$(pwd)/tools:$PATH
           '';
         };
       });
